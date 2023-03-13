ca098e0b2bc78f15a1ce6c2d6a3508ae555734be
AlexeyLavrentev <105936590+AlexeyLavrentev@users.noreply.github.com>
2023-03-13 09:14:07 +0500

    Add files via upload

---
+++ Seminar_04/ReadNotepad.java
@@ -0,0 +1,17 @@
+package Seminar_04;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadNotepad {
+    public void readFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("NotePad.csv"));
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+        br.close();
+    }
+}
---
+++ Seminar_04/YesterdayYet.java
@@ -0,0 +1,15 @@
+package Seminar_04;
+
+import java.time.LocalDate;
+
+public class YesterdayYet extends Task {
+
+    public YesterdayYet(LocalDate data, String node, String deadLineTime, String deadLineDate, String avtor) {
+        super(data, node, deadLineTime, deadLineDate, avtor);
+    }
+
+    @Override
+    public String toString() {
+        return "!!! High " + super.toString();
+    }
+}
